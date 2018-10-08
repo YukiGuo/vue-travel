f7e09bc8d4adc28ee02c11a3b9ab92a529cb4e6d
Yuki <834517986@qq.com>
2018-10-08 22:53:23 +0800

    城市页面搜索框制作

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,12 +1,17 @@
 <template>
-	<city-head></city-head>
+	<div>
+	  <city-head></city-head>
+	  <city-search></city-search>
+	</div>  
 </template>
 <script >
 import CityHead from './components/Head.vue'
+import CitySearch from './components/Search.vue'
   export default { 
       name:'City',
       components: {
       	CityHead,
+      	CitySearch
       }
 	}
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="wrapper">
+	<input class="search" placeholder="输入城市名或拼音">
+</div>
+</template>
+<script >
+export default { 
+      name:'CitySearch',
+	}
+</script>
+
+<style lang ="stylus" scoped>
+@import '~style/varibles.styl'
+  .wrapper
+    height:.72rem
+    background-color:$bgColor
+    padding:0 .1rem
+    .search
+      box-sizing:border-box
+      width:100%
+      height:.62rem
+      padding:0 .1rem
+      line-height:.62rem
+      border-radius:.06rem
+      text-align:center
+      color:#666
+</style>
