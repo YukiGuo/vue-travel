1c96dd2281e57be2dcba8bdf46566b983738df22
Yuki <834517986@qq.com>
2018-10-09 21:21:13 +0800

    城市列表页面和右侧字母表，ajax 数据接入完成

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,52 @@
 	<div>
 	  <city-head></city-head>
 	  <city-search></city-search>
+    <city-list :city="city" :hot="hot"></city-list>
+    <city-alphbet :city="city"></city-alphbet>
 	</div>  
 </template>
 <script >
+import axios from 'axios'
 import CityHead from './components/Head.vue'
 import CitySearch from './components/Search.vue'
+import CityList from './components/List.vue'
+import CityAlphbet from './components/Alphbet.vue'
   export default { 
       name:'City',
       components: {
       	CityHead,
-      	CitySearch
-      }
+      	CitySearch,
+        CityList,
+        CityAlphbet
+      },
+      data () {
+        return {
+          city:{},
+          hot:[]
+        }
+      },
+      methods:{
+        getCityInfo (){
+          axios.get('/api/citylist.json').then(this.getCityInfoSucc)
+        },
+        getCityInfoSucc (res) {
+          console.log(res)
+          console.log(res.data)
+          console.log(res.data.data)
+          console.log(res.data.data.cities)
+          res =res.data
+          if (res.ret && res.data){
+            this.city = res.data.cities
+            this.hot = res.data.hotCities
+          }
+          else{
+            alert("wrong")
+          }
+        }
+      },
+       mounted () {
+        this.getCityInfo()
+      },
 	}
 </script>
 
---
+++ src/pages/city/components/ALphbet.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <ul class="list" >
+    <li class="item" v-for = "(item,key) of city" :key="key">{{key}}</li>
+  </ul>
+</div> 
+</template>
+<script >
+  export default { 
+      name:'CityAlphbet',
+      props:{
+        city:Object
+      }
+	}
+</script>
+
+<style lang ="stylus" scoped>
+@import "~style/varibles.styl"
+  .list
+    display:flex
+    flex-direction:column
+    justify-content:center
+    position:absolute
+    top:2.8rem
+    right:0
+    bottom:0
+    width:.4rem
+    .item
+      line-height:.4rem
+      color:$bgColor
+      text-align:center
+
+
+</style>
